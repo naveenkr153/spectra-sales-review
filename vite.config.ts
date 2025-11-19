752e511579ad6d231e14a6935d5420b6b911b491
Naveen Kumar <79682298+naveenkr153@users.noreply.github.com>
2025-11-19 14:38:18 +0530

    base /
    Set base path for GitHub Pages deployment.

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
       server: {
+        base: './',   // <-- REQUIRED for GitHub Pages
         port: 3000,
         host: '0.0.0.0',
       },
